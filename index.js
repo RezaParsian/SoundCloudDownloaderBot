29c26f79fdec931d0391c7774900f5b4a2f78179
unknown <rezaparsian76@gmail.com>
2025-01-16 00:20:20 +0330

    add try catch send error message to admin.

--- index.js
+++ index.js
@@ -3,17 +3,29 @@ import TelegramBot from "node-telegram-bot-api";
 import {download, isUrl} from "./src/kernel.js";
 
 const TOKEN = process.env.BOT_TOKEN;
+const ADMIN = process.env.BOT_ADMIN;
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
 bot.on('message',async (msg) => {
-  try{
-    if (!isUrl(msg.text))
-      return bot.sendMessage(msg.chat.id, 'please send a valid url');
+  const message = msg.text;
+  const chatId = msg.chat.id;
 
-    const file = await download(msg.text);
-    bot.sendAudio(msg.chat.id, `./${file}`);
-  }catch (e){
-    bot.sendMessage(msg.chat.id, '404 - not found');
-  }
+  if (!isUrl(message))
+    return bot.sendMessage(chatId, 'لطفا یک لینک معتبر ارسال کنید.');
+
+  bot.sendChatAction(chatId, 'upload_audio').then(async () => {
+    try {
+      const file = await download(message);
+      bot.sendAudio(chatId, `./${file}`);
+    } catch (e) {
+      bot.sendMessage(chatId, 'خطایی رخ داد، لطفا بعدا تلاش کنید.');
+
+      bot.sendMessage(ADMIN, JSON.stringify({
+        message,
+        chatId,
+        error: e.message,
+      }));
+    }
+  });
 });
