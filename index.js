976f5cc20506b29b259f81974ef943a251855fdf
RezaParsian <Rezaparsian76@gmail.com>
2025-01-18 14:20:06 +0330

    add webhook.

--- index.js
+++ index.js
@@ -1,12 +1,82 @@
 import 'dotenv/config'
 import fs from "fs";
 import TelegramBot from "node-telegram-bot-api";
+import {serve} from '@hono/node-server'
+import {Hono} from 'hono'
 import {download, isUrl} from "./src/kernel.js";
 
 const TOKEN = process.env.BOT_TOKEN;
 const ADMIN = process.env.BOT_ADMIN;
+const PORT = process.env.PORT || 3000;
 
-const bot = new TelegramBot(TOKEN, { polling: true });
+const app = new Hono()
+const bot = new TelegramBot(TOKEN, {polling: false});
+
+
+app.all('/', async (c) => {
+  const json = await c.req.json();
+
+  bot.processUpdate(json);
+
+  return c.body('', 200);
+});
+
+async function isUserRegister(chatId) {
+  return await bot.getChatMember('@Rp1376', chatId).then(data => {
+    console.log(data)
+    return data.status === 'member';
+  });
+}
+
+bot.onText(/\/start/, async (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendChatAction(chatId, 'typing')
+      .then(async () => {
+        await bot.sendMessage(chatId, 'سلام خوش امدی');
+
+        if (!await isUserRegister(chatId))
+          bot.sendMessage(chatId, 'خب قبل از هرکاری باید عضو این کانال بشی @Rp1376', {
+            reply_markup: {
+              inline_keyboard: [
+                [
+                  {
+                    text: "عضو شدم",
+                    callback_data: "re-check",
+                  },
+                ],
+              ],
+            },
+          })
+
+      });
+});
+
+
+bot.on('callback_query', async (callbackQuery) => {
+  const msg = callbackQuery.message;
+  const chatId = msg.chat.id;
+  const action = callbackQuery.data;
+  console.log(callbackQuery)
+  let text = undefined;
+
+  switch (action) {
+    case 're-check':
+      if (!await isUserRegister(chatId))
+        text = 'هنوز عضو نشدی که شیطون 😁';
+      else {
+        text = 'ایول بابا <3'
+        await bot.deleteMessage(chatId, msg.message_id);
+        await bot.sendMessage(chatId, 'خب لینک آهنگ رو برام بفرست تا سریع برات دانلودش کنم');
+      }
+      break;
+    default:
+      await bot.sendMessage(chatId, 'نفهمیدم چی شد 😒')
+  }
+
+  if (text)
+    await bot.answerCallbackQuery(callbackQuery.id, text);
+})
 
 bot.on('message',async (msg) => {
   const message = msg.text;
@@ -39,8 +109,13 @@ bot.on('message',async (msg) => {
       await bot.sendMessage(ADMIN, JSON.stringify({
         message,
         chatId,
-        error: e.message,
+        error: e.message(),
       }));
     }
   });
+});
+
+serve({
+  fetch: app.fetch,
+  port: PORT
 });
